ed39ee0a4debffd97568dbc18b479ed4b3fb9f48
Egor Pozharov <chedius232@gmail.com>
2021-11-17 10:12:27 -0800

    init

---
+++ app/javascript/components/Art.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Art from "./Src/Index.jsx";
+
+const Art = () => {
+return <Index />;
+};
+
+export default Art;
---
+++ app/javascript/components/Src/Index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Index.scss";
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+
+const Index = props => {
+return <div>blabla</div>
+}
+
+export default Index;
